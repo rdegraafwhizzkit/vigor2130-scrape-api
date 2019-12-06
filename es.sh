65fd74b42fc05740d1b1614cfc8df089e66eb93a
Rutger de Graaf <rutger.de.graaf@randstadgroep.nl>
2019-12-06 14:24:49 +0100

    ADded elasticsearch loading

---
+++ es.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -XDELETE http://localhost:9200/vigor2130
+curl -XPUT http://localhost:9200/vigor2130
+
+curl -XPUT http://localhost:9200/vigor2130/_mapping \
+-H "Content-Type: application/json" \
+--data '{"properties":{"computer_name":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"expire_minutes":{"type":"float"},"ip_address":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"mac_address":{"type":"text","fields":{"keyword":{"type":"keyword","ignore_above":256}}},"rx_rate_kbs":{"type":"float"},"timestamp":{"type":"date","format":"epoch_second"},"tx_rate_kbs":{"type":"float"}}}'

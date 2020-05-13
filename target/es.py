59ed030cfdc5d4375b6f1bf45a8c7ec8079fc9ca
Rutger de Graaf <rutger@whizzkit.nl>
2020-05-13 21:37:50 +0200

    Moved to bulk API in index_objects function

--- target/es.py
+++ target/es.py
@@ -1,19 +1,25 @@
-from elasticsearch import Elasticsearch
-from json import dumps
+from elasticsearch import Elasticsearch, helpers
 from conf.config import config
 from helper.global_helpers import sha224
 
 
-def index_objects(index, objects, doc_type='_doc', id_function=lambda x: sha224(x)):
+def index_objects(index, objects, doc_type='_doc', id_function=lambda x: sha224(x), hosts=None):
+    if hosts is None:
+        hosts = [{'host': 'localhost', 'port': 9200}]
     if type(objects) == dict:
-        return index_objects(index, [objects], doc_type, id_function)
+        return index_objects(index, [objects], doc_type, id_function, hosts)
     elif type(objects) == list:
-        es = Elasticsearch(hosts=config['es']['hosts'])
+        es = Elasticsearch(hosts=hosts)
         for o in objects:
-            try:
-                es.index(index=index, doc_type=doc_type, body=o, id=id_function(o))
-            except Exception as ex:
-                print('Could not index object {}: {}'.format(dumps(o), ex))
-        return len(objects)
+            o.update({
+                '_id': id_function(o),
+                '_type': doc_type,
+                '_index': index
+            })
+        success, errors = helpers.bulk(
+            es,
+            objects
+        )
+        return success
     else:
         raise Exception('Unable to load objects with type ' + str(type(objects)))
